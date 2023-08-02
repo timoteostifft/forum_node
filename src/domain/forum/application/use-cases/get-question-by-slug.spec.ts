799bd7b5d12020b04b0d00fa7a238c4738ec71fa
timoteostifft <timoteostifft@gmail.com>
2023-08-02 16:58:05 -0300

    feat: add question factory

--- src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
+++ src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
@@ -4,6 +4,7 @@ import { GetQuestionBySlugUseCase } from './get-question-by-slug'
 import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
 import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { makeQuestion } from 'test/factories/make-question'
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository
 let sut: GetQuestionBySlugUseCase
@@ -15,11 +16,8 @@ describe('Get Question By Slug', () => {
   })
 
   it('should be able to get a question by slug', async () => {
-    const newQuestion = Question.create({
-      authorId: new UniqueEntityID(),
-      title: 'Example Question',
-      slug: Slug.create('example-question'),
-      content: 'Example content',
+    const newQuestion = makeQuestion({
+      slug: Slug.create('example-question')
     })
 
     await inMemoryQuestionsRepository.create(newQuestion)
