f9f08626e670d0f1f56cd4b6115ee86b0474aa5c
timoteostifft <timoteostifft@gmail.com>
2023-08-04 11:49:18 -0300

    feat: add fetch question answers

--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from "@/core/repositories/pagination-params"
 import { AnswersRepository } from "@/domain/forum/application/repositories/answers-repository"
 import { Answer } from "@/domain/forum/enterprise/entities/answer"
 
@@ -14,15 +15,23 @@ export class InMemoryAnswersRepository implements AnswersRepository {
     return answer
   }
 
+  async findManyByQuestionId(questionId: string, { page }: PaginationParams): Promise<Answer[]> {
+    const answers = this.items
+      .filter(item => item.questionId.toString() === questionId)
+      .slice((page - 1) * 20, page * 20)
+
+    return answers
+  }
+
+  
+  async create(answer: Answer): Promise<void> {
+    this.items.push(answer)
+  }
+
   async delete(answer: Answer): Promise<void> {
     const itemIndex = this.items.findIndex((item) => item.id === answer.id)
 
     this.items.splice(itemIndex, 1)
-  }
-
-
-  async create(answer: Answer): Promise<void> {
-    this.items.push(answer)
   }
 
   async save(answer: Answer): Promise<void> {
