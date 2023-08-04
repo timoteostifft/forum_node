10b8674e1c3f72815f013ff6da9354fba9cc314c
timoteostifft <timoteostifft@gmail.com>
2023-08-04 15:46:15 -0300

    feat: add delete answer comment

--- src/domain/forum/application/use-cases/comment-on-answer.spec.ts
+++ src/domain/forum/application/use-cases/comment-on-answer.spec.ts
@@ -1,7 +1,7 @@
 import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
 import { makeAnswer } from 'test/factories/make-answer'
 import { CommentOnAnswerUseCase } from '@/domain/forum/application/use-cases/comment-on-answer'
-import { InMemoryAnswerCommentsRepository } from 'test/repositories/in-memory-answers-comments-repository'
+import { InMemoryAnswerCommentsRepository } from 'test/repositories/in-memory-answer-comments-repository'
 
 let inMemoryAnswersRepository: InMemoryAnswersRepository
 let inMemoryAnswerCommentsRepository: InMemoryAnswerCommentsRepository
