b2ab307b72e337ad2a7eca21edd76a14c8429493
timoteostifft <timoteostifft@gmail.com>
2023-08-04 11:15:26 -0300

    feat: add fetch recent questions with pagination

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -1,12 +1,9 @@
+import { PaginationParams } from "@/core/repositories/pagination-params";
 import { QuestionsRepository } from "@/domain/forum/application/repositories/questions-repository";
 import { Question } from "@/domain/forum/enterprise/entities/question";
 
 export class InMemoryQuestionsRepository implements QuestionsRepository {
   public items: Question[] = []
-
-  async create(question: Question): Promise<void> {
-    this.items.push(question)
-  }
 
   async findBySlug(slug: string): Promise<Question | null> {
     const question = this.items.find((item) => item.slug.value === slug)
@@ -26,6 +23,18 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
     }
 
     return question
+  }
+
+  async findManyRecent({ page }: PaginationParams): Promise<Question[]> {
+    const questions = this.items
+      .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
+      .slice((page - 1) * 20, page * 20)
+
+    return questions
+  }
+
+  async create(question: Question): Promise<void> {
+    this.items.push(question)
   }
 
   async delete(question: Question): Promise<void> {
