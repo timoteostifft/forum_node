1fa4ff3d43646916fc7b95268f8bb36d0b12eba0
timoteostifft <timoteostifft@gmail.com>
2023-08-04 08:01:53 -0300

    feat: add edit question

--- src/domain/forum/application/use-cases/edit-question.spec.ts
+++ src/domain/forum/application/use-cases/edit-question.spec.ts
@@ -1,19 +1,19 @@
 
-import { DeleteQuestionUseCase } from './delete-question'
+import { EditQuestionUseCase } from './edit-question'
 import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
 import { makeQuestion } from 'test/factories/make-question'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository
-let sut: DeleteQuestionUseCase
+let sut: EditQuestionUseCase
 
-describe('Delete Question', () => {
+describe('Edit Question', () => {
   beforeEach(() => {
     inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
-    sut = new DeleteQuestionUseCase(inMemoryQuestionsRepository)
+    sut = new EditQuestionUseCase(inMemoryQuestionsRepository)
   })
 
-  it('should be able to delete a question', async () => {
+  it('should be able to edit a question', async () => {
     const newQuestion = makeQuestion(
       {
         authorId: new UniqueEntityID('author-1'),
@@ -24,14 +24,19 @@ describe('Delete Question', () => {
     await inMemoryQuestionsRepository.create(newQuestion)
 
     await sut.execute({
-      questionId: 'question-1',
+      questionId: newQuestion.id.toValue(),
       authorId: 'author-1',
+      title: "Pergunta teste",
+      content: "Conteúdo teste"
     })
 
-    expect(inMemoryQuestionsRepository.items).toHaveLength(0)
+    expect(inMemoryQuestionsRepository.items[0]).toMatchObject({
+      title: "Pergunta teste",
+      content: "Conteúdo teste"
+    })
   })
 
-  it('should not be able to delete a question from another user', async () => {
+  it('should not be able to edit a question from another user', async () => {
     const newQuestion = makeQuestion(
       {
         authorId: new UniqueEntityID('author-1'),
@@ -43,8 +48,10 @@ describe('Delete Question', () => {
 
     expect(() => {
       return sut.execute({
-        questionId: 'question-1',
+        questionId: newQuestion.id.toValue(),
         authorId: 'author-2',
+        title: "Pergunta teste",
+        content: "Conteúdo teste"
       })
     }).rejects.toBeInstanceOf(Error)
   })
