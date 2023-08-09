0634796ba83c1e376d239aac64787f2e24b015c5
timoteostifft <timoteostifft@gmail.com>
2023-08-09 07:45:44 -0300

    feat: add question attachments cascade delete

--- test/repositories/in-memory-question-attachments-repository.ts
+++ test/repositories/in-memory-question-attachments-repository.ts
@@ -1,9 +1,7 @@
 import { QuestionAttachmentsRepository } from '@/domain/forum/application/repositories/question-attachments-repository'
 import { QuestionAttachment } from '@/domain/forum/enterprise/entities/question-attachment'
 
-export class InMemoryQuestionAttachmentsRepository
-  implements QuestionAttachmentsRepository
-{
+export class InMemoryQuestionAttachmentsRepository implements QuestionAttachmentsRepository {
   public items: QuestionAttachment[] = []
 
   async findManyByQuestionId(questionId: string) {
@@ -12,5 +10,13 @@ export class InMemoryQuestionAttachmentsRepository
     )
 
     return questionAttachments
+  }
+
+  async deleteManyByQuestionId(questionId: string): Promise<void> {
+    const questionAttachments = this.items.filter(
+      (item) => item.questionId.toString() !== questionId,
+    )
+
+    this.items = questionAttachments
   }
 }
