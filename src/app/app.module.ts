710f861c2b0d57886a42ef8107c7e6b65ed146c9
timoteostifft <timoteostifft@gmail.com>
2023-09-21 14:35:16 -0300

    feat: add fetch recent questions controller

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { envSchema } from '@/env'
 import { AuthModule } from './auth/auth.module'
 import { AuthenticateController } from './controllers/authenticate.controller'
 import { CreateQuestionController } from './controllers/create-question.controller'
+import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller'
 
 @Module({
   imports: [
@@ -18,7 +19,8 @@ import { CreateQuestionController } from './controllers/create-question.controll
   controllers: [
     CreateAccountController,
     AuthenticateController,
-    CreateQuestionController
+    CreateQuestionController,
+    FetchRecentQuestionsController
   ],
   providers: [PrismaService],
 })
