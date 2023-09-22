d36ee047ec6a8baa909f484e592fdc9b412cb6bf
timoteostifft <timoteostifft@gmail.com>
2023-09-22 17:42:18 -0300

    feat: add question domain mapper

--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -15,7 +15,7 @@ export interface QuestionProps {
   attachments: QuestionAttachmentList
   slug: Slug
   createdAt: Date
-  updatedAt?: Date
+  updatedAt?: Date | null
 }
 
 export class Question extends AggregateRoot<QuestionProps> {
