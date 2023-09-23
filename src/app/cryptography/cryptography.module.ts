0670bbb8e1431e798dbfd52914b849f39c1aa9cc
timoteostifft <timoteostifft@gmail.com>
2023-09-23 01:31:39 -0300

    feat: add cryptography in infra layer

---
+++ src/app/cryptography/cryptography.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common'
+import { BcryptHasher } from './bcrypt-hasher'
+import { Encrypter } from '@/domain/forum/application/cryptography.ts/encrypter'
+import { HashComparer } from '@/domain/forum/application/cryptography.ts/hash-comparer'
+import { HashGenerator } from '@/domain/forum/application/cryptography.ts/hash-generator'
+import { JwtEncrypter } from './jwt-encrypter'
+
+@Module({
+  providers: [
+    { provide: Encrypter, useClass: JwtEncrypter },
+    { provide: HashComparer, useClass: BcryptHasher },
+    { provide: HashGenerator, useClass: BcryptHasher },
+  ],
+  exports: [Encrypter, HashComparer, HashGenerator],
+})
+export class CryptographyModule {}
