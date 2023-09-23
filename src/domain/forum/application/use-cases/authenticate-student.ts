fbcdd9906df4a992534baf8fc587a0b8776862b4
timoteostifft <timoteostifft@gmail.com>
2023-09-23 01:26:46 -0300

    feat: add register and authenticate student on domain layer

---
+++ src/domain/forum/application/use-cases/authenticate-student.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@nestjs/common'
+import { StudentsRepository } from '../repositories/students-repository'
+import { Either, left, right } from '@/core/types/either'
+import { Encrypter } from '../cryptography.ts/encrypter'
+import { HashComparer } from '../cryptography.ts/hash-comparer'
+import { WrongCredentialsError } from './errors/wrong-credentials-error'
+
+interface AuthenticateStudentUseCaseRequest {
+  email: string
+  password: string
+}
+
+type AuthenticateStudentUseCaseResponse = Either<
+  WrongCredentialsError,
+  {
+    accessToken: string
+  }
+>
+
+@Injectable()
+export class AuthenticateStudentUseCase {
+  constructor(
+    private studentsRepository: StudentsRepository,
+    private hashComparer: HashComparer,
+    private encrypter: Encrypter,
+  ) {}
+
+  async execute({
+    email,
+    password,
+  }: AuthenticateStudentUseCaseRequest): Promise<AuthenticateStudentUseCaseResponse> {
+    const student = await this.studentsRepository.findByEmail(email)
+
+    if (!student) {
+      return left(new WrongCredentialsError())
+    }
+
+    const isPasswordValid = await this.hashComparer.compare(
+      password,
+      student.password,
+    )
+
+    if (!isPasswordValid) {
+      return left(new WrongCredentialsError())
+    }
+
+    const accessToken = await this.encrypter.encrypt({
+      sub: student.id.toString(),
+    })
+
+    return right({
+      accessToken,
+    })
+  }
+}
