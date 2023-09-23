b07eb94e99be70532d66a9c16e610f05c5ce4d44
timoteostifft <timoteostifft@gmail.com>
2023-09-23 00:27:38 -0300

    feat: implement create question domain use case in infra layer

--- src/app/database/database.module.ts
+++ src/app/database/database.module.ts
@@ -1,16 +1,20 @@
 import { Module } from '@nestjs/common'
 import { PrismaService } from './prisma/prisma.service'
-import { PrismaAnswerCommentsRepository } from './prisma/repositories/prisma-answer-comments-repository'
-import { PrismaAnswersRepository } from './prisma/repositories/prisma-answers-repository'
-import { PrismaQuestionCommentsRepository } from './prisma/repositories/prisma-question-comments-repository'
-import { PrismaQuestionAttachmentsRepository } from './prisma/repositories/prisma-questions-attachments-repository'
 import { PrismaQuestionsRepository } from './prisma/repositories/prisma-questions-repository'
+import { PrismaQuestionCommentsRepository } from './prisma/repositories/prisma-question-comments-repository'
+import { PrismaAnswersRepository } from './prisma/repositories/prisma-answers-repository'
+import { PrismaAnswerCommentsRepository } from './prisma/repositories/prisma-answer-comments-repository'
 import { PrismaAnswerAttachmentsRepository } from './prisma/repositories/prisma-answer-attachments-repository'
+import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
+import { PrismaQuestionAttachmentsRepository } from './prisma/repositories/prisma-questions-attachments-repository'
 
 @Module({
   providers: [
     PrismaService,
-    PrismaQuestionsRepository,
+    {
+      provide: QuestionsRepository,
+      useClass: PrismaQuestionsRepository,
+    },
     PrismaQuestionCommentsRepository,
     PrismaQuestionAttachmentsRepository,
     PrismaAnswersRepository,
@@ -19,7 +23,7 @@ import { PrismaAnswerAttachmentsRepository } from './prisma/repositories/prisma-
   ],
   exports: [
     PrismaService,
-    PrismaQuestionsRepository,
+    QuestionsRepository,
     PrismaQuestionCommentsRepository,
     PrismaQuestionAttachmentsRepository,
     PrismaAnswersRepository,
--- src/app/http/controllers/create-question.controller.ts
+++ src/app/http/controllers/create-question.controller.ts
@@ -1,10 +1,10 @@
 import { Body, Controller, Post, UseGuards } from '@nestjs/common'
 import { z } from 'zod'
+import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/create-question'
 import { ZodValidationPipe } from '../pipes/zod-validation-pipe'
-import { CurrentUser } from '../../auth/current-user-decorator'
-import { JwtAuthGuard } from '../../auth/jwt-auth.guard'
-import { UserPayload } from '../../auth/jwt-strategy'
-import { PrismaService } from '../../database/prisma/prisma.service'
+import { JwtAuthGuard } from '@/app/auth/jwt-auth.guard'
+import { CurrentUser } from '@/app/auth/current-user-decorator'
+import { UserPayload } from '@/app/auth/jwt-strategy'
 
 const createQuestionBodySchema = z.object({
   title: z.string(),
@@ -18,7 +18,7 @@ type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>
 @Controller('/questions')
 @UseGuards(JwtAuthGuard)
 export class CreateQuestionController {
-  constructor(private prisma: PrismaService) {}
+  constructor(private createQuestion: CreateQuestionUseCase) {}
 
   @Post()
   async handle(
@@ -28,24 +28,11 @@ export class CreateQuestionController {
     const { title, content } = body
     const userId = user.sub
 
-    const slug = this.convertToSlug(title)
-
-    await this.prisma.question.create({
-      data: {
-        authorId: userId,
-        title,
-        content,
-        slug,
-      },
+    await this.createQuestion.execute({
+      title,
+      content,
+      authorId: userId,
+      attachmentsIds: [],
     })
-  }
-
-  private convertToSlug(title: string): string {
-    return title
-      .toLowerCase()
-      .normalize('NFD')
-      .replace(/[\u0300-\u036f]/g, '')
-      .replace(/[^\w\s-]/g, '')
-      .replace(/\s+/g, '-')
   }
 }
