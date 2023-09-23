5f0173edaa9d3b45853759d48da2f44bf0e29bb7
timoteostifft <timoteostifft@gmail.com>
2023-09-23 02:27:29 -0300

    feat: add env module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,19 @@
 import { Module } from '@nestjs/common'
-import { PrismaService } from './database/prisma/prisma.service'
-import { CreateAccountController } from './http/controllers/create-account.controller'
 import { ConfigModule } from '@nestjs/config'
-import { envSchema } from '@/app/env'
 import { AuthModule } from './auth/auth.module'
 import { HttpModule } from './http/http.module'
+import { EnvModule } from './env/env.module'
+import { envSchema } from './env'
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       validate: (env) => envSchema.parse(env),
-      isGlobal: true
-      }),
+      isGlobal: true,
+    }),
     AuthModule,
-    HttpModule
+    HttpModule,
+    EnvModule,
   ],
 })
 export class AppModule {}
--- src/app/main.ts
+++ src/app/main.ts
@@ -2,12 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { Env } from '@/app/env';
+import { EnvService } from './env/env.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const configService = app.get<ConfigService<Env, true>>(ConfigService);
-  const port = configService.get('PORT', { infer: true })
+  const configService = app.get(EnvService)
+  const port = configService.get('PORT')
 
   await app.listen(port);
 }
