51890d3beda29c6c7772de8a601cfca272604a6f
timoteostifft <timoteostifft@gmail.com>
2023-09-24 14:09:28 -0300

    feat: add delete answer comment controller

--- src/domain/forum/application/use-cases/delete-answer-comment.ts
+++ src/domain/forum/application/use-cases/delete-answer-comment.ts
@@ -2,6 +2,7 @@ import { left, right, Either } from '@/core/types/either'
 import { AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comments-repository'
 import { ResourceNotFoundError } from '../../../../core/errors/resource-not-found-error'
 import { NotAllowedError } from '../../../../core/errors/not-allowed-error'
+import { Injectable } from '@nestjs/common'
 
 interface DeleteAnswerCommentUseCaseRequest {
   authorId: string
@@ -13,6 +14,7 @@ type DeleteAnswerCommentUseCaseResponse = Either<
   null
 >
 
+@Injectable()
 export class DeleteAnswerCommentUseCase {
   constructor(private answerCommentsRepository: AnswerCommentsRepository) {}
 
